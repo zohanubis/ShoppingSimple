61055e169a882a3b8d0d76204c27e242cc9a7d8d
Zohanubis <zohanubis2509@gmail.com>
2024-09-30 17:43:30 +0700

    Cau truc

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '4000'
-        // pathname: '/photos/**'
-      }
-    ]
-  },
-  logging: {
-    fetches: {
-      fullUrl: true
-    }
-  }
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
